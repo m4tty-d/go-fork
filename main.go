76ae4deca5ca3c5fff07a2e2b5e5589f2566e8bf
Alex <tothalex097@gmail.com>
2018-10-08 23:53:10 +0200

    time sync, async version added

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"gitlab.com/chess-fork/go-fork/handlers"
+	"gitlab.com/chess-fork/go-fork/socketpool"
 )
 
 func main() {
 	handlers.Upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
+
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for range ticker.C {
+			socketpool.VerifyTime()
+		}
+	}()
 
 	http.HandleFunc("/ws", handlers.Handler)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8089", nil))
